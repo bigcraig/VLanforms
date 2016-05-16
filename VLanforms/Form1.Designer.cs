832567ce8bf8b94bc98e37254a1e588b074af592
Craig Woollett <craigwoollett@outlook.com>
2016-05-16 17:08:12 +1000

    Initial commit gui fucntioning no ACS or resposne checking

---
+++ VLanforms/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace VLanforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.configureModem = new System.Windows.Forms.Button();
+            this.usernameControl = new System.Windows.Forms.TextBox();
+            this.passwordControl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // configureModem
+            // 
+            this.configureModem.Location = new System.Drawing.Point(85, 340);
+            this.configureModem.Name = "configureModem";
+            this.configureModem.Size = new System.Drawing.Size(124, 44);
+            this.configureModem.TabIndex = 0;
+            this.configureModem.UseVisualStyleBackColor = true;
+            this.configureModem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // usernameControl
+            // 
+            this.usernameControl.Location = new System.Drawing.Point(43, 131);
+            this.usernameControl.Name = "usernameControl";
+            this.usernameControl.Size = new System.Drawing.Size(109, 20);
+            this.usernameControl.TabIndex = 1;
+            this.usernameControl.TextChanged += new System.EventHandler(this.usernameControl_TextChanged);
+            // 
+            // passwordControl
+            // 
+            this.passwordControl.Location = new System.Drawing.Point(43, 214);
+            this.passwordControl.Name = "passwordControl";
+            this.passwordControl.Size = new System.Drawing.Size(109, 20);
+            this.passwordControl.TabIndex = 2;
+            this.passwordControl.TextChanged += new System.EventHandler(this.passwordControl_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "This utility will configure VDSL with VLAN 100";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 456);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordControl);
+            this.Controls.Add(this.usernameControl);
+            this.Controls.Add(this.configureModem);
+            this.Name = "Form1";
+            this.Text = "Netcomm Vlan 100";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button configureModem;
+        private System.Windows.Forms.TextBox usernameControl;
+        private System.Windows.Forms.TextBox passwordControl;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ VLanforms/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleBrowser;
+using System.Text.RegularExpressions;
+
+namespace VLanforms
+{
+    public partial class Form1 : Form
+    {
+        string PPPusername, PPPpassword;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       static string getSessionID(string html)
+        {      // static since we dont need to instance another program class to run it
+
+            string _pattern = @"var sessionKey='([^']+)';";
+            Match match = Regex.Match(html, _pattern);
+            return match.Groups[1].Value;
+
+        }
+
+        static void vlanConfiguration(string PPPusername, string PPPpassword)
+        {
+
+            string _PPPusername = PPPusername;
+            string _PPPpassword = PPPpassword;
+            var modem = new Browser();
+
+            var modemURL = "http://192.168.20.1/qinetsetup.html";
+            modem.Navigate(modemURL);
+            //    Console.Write("get url");
+            //    modem.BasicAuthenticationLogin("Broadband Router","admin","admin");
+            modem.BasicAuthenticationLogin("192.168.20.1", "admin", "admin");
+
+
+            modem.Navigate(modemURL);
+            //     Console.Write(modem.CurrentHtml);
+            var html = modem.CurrentHtml;
+            string sessionKey = getSessionID(html);
+            // post wlan type
+            modemURL = "http://192.168.20.1/qvdslwanmode.cgi?wanType=2&sessionKey=" + sessionKey;
+            modem.Navigate(modemURL);
+            html = modem.CurrentHtml;
+            sessionKey = getSessionID(html);
+            modemURL = "http://192.168.20.1/qvdslppp.cgi?ntwkPrtcl=0&enblOnDemand=0&pppTimeOut=0&enblv4=1&useStaticIpAddress=0&pppIpExtension=0&enblFirewall=1&enblNat=1&enblIgmp=1&keepalive=1&keepalivetime=5&alivemaxfail=30&enVlanMux=1&vlanMuxId=69&vlanMuxPr=0&enblPppDebug=0&maxMtu=1492&keepalive=0&enblv6=0&pppAuthErrorRetry=0&pppAuthMethod=0&sessionKey=" + sessionKey;
+            modem.Navigate(modemURL);
+            html = modem.CurrentHtml;
+            sessionKey = getSessionID(html);
+            modemURL = "http://192.168.20.1/qsetup.cmd?pppUserName="
+                + _PPPusername + "&pppPassword="
+                + _PPPpassword +"&portId=0&ptmPriorityNorm=1&ptmPriorityHigh=1&connMode=1&burstsize=3000&enblQos=1&grpPrec=8&grpAlg=WRR&grpWght=1&prec=8&alg=WRR&wght=1&sessionKey=" + sessionKey;
+            modem.Navigate(modemURL);
+
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            configureModem.Text = "Configuring Modem please wait";
+            vlanConfiguration(PPPusername,PPPpassword);
+            configureModem.Text = "Configured!!!!!!";
+        }
+
+        private void usernameControl_TextChanged(object sender, EventArgs e)
+        {
+            PPPusername = usernameControl.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passwordControl_TextChanged(object sender, EventArgs e)
+        {
+            PPPpassword = passwordControl.Text;
+
+        }
+    }
+}
